7e9e30ed7ac0c716c910ad80f87c5ca6c549c375
Arnaud <arnaud.lente@gmail.com>
2016-09-06 16:06:39 +0200

    06/09/16

--- Developper des pages web/JavaScript/contact.js
+++ Developper des pages web/JavaScript/contact.js
@@ -1,13 +1,17 @@
 // regex
-var regcara = "^+[A-Za-z]$";
-var regcp = "^[0-9]{5}$";
-var regmail = "/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/";
+// var regcara = /^+[A-Za-z]$/;
+// var regcp = "^[0-9]{5}$";
+// var regmail = "/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/";
 
 //controle de saisie
 
 var regex = function(controle)
 {
-	if (controle.target.id == "société")
+	var regcara = /^[A-Za-z]+$/;
+	var regcp = /^((0[1-9])|([1-8][0-9])|(9[0-8])|(2A)|(2B))[0-9]{3}$/;
+	var regmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+	if (controle.target.id == "societe")
 		{
 			if (controle.target.value =="")
 			{
@@ -29,7 +33,7 @@ var regex = function(controle)
 				alert("Personne non valide !");		
 			}
 		}
-	else if (controle.target.id == "adresse")
+	else if (controle.target.id == "email")
 		{
 			if (controle.target.value =="")
 			{
@@ -53,10 +57,24 @@ var regex = function(controle)
 		}
 }
 
-// bouton
+document.getElementById("societe").addEventListener("blur",regex);
+document.getElementById("pac").addEventListener("blur",regex);
+document.getElementById("CP").addEventListener("blur",regex);
+document.getElementById("ville").addEventListener("blur",regex);
+document.getElementById("email").addEventListener("blur",regex);
 
-var element = document.getElementById("Valider");
-element.addEventListener("blur",regex);
+
+
+
+
+function affiche()
+{
+	var elt = document.getElementById("language").value;
+	document.getElementById("ETP").value += elt + "\n";
+}
+
+var liste = document.getElementById("language");
+liste.addEventListener("change",affiche);
 
 
 
